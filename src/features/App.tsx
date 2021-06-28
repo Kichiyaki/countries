282b997f1e06e5549e4b6aeff6ae80ead8755ff1
Kichiyaki <xyztojajestem@gmail.com>
2021-06-28 18:42:52 +0200

    add a new component - CssBaseline, add @material-ui/styles

--- src/features/App.tsx
+++ src/features/App.tsx
@@ -1,14 +1,19 @@
+import { Fragment } from "react";
 import { Switch, Route as RRDRoute } from "react-router-dom";
+import CssBaseline from "common/CssBaseline/CssBaseline";
 import IndexPage from "./IndexPage/IndexPage";
 import { Route } from "../config/routing";
 
 function App() {
   return (
-    <Switch>
-      <RRDRoute path={Route.IndexPage} exact>
-        <IndexPage />
-      </RRDRoute>
-    </Switch>
+    <Fragment>
+      <Switch>
+        <RRDRoute path={Route.IndexPage} exact>
+          <IndexPage />
+        </RRDRoute>
+      </Switch>
+      <CssBaseline />
+    </Fragment>
   );
 }
 
--- src/features/IndexPage/IndexPage.tsx
+++ src/features/IndexPage/IndexPage.tsx
@@ -1,5 +1,26 @@
+import { makeStyles } from "@material-ui/styles";
+
 const IndexPage = () => {
-  return <div>IndexPage</div>;
+  const classes = useStyles();
+  return (
+    <div className={classes.main}>
+      <h1 className={classes.h1}>Lista kontynentów</h1>
+    </div>
+  );
 };
+
+const useStyles = makeStyles(() => {
+  return {
+    main: {
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      minHeight: "100vh",
+    },
+    h1: {
+      fontSize: "5rem",
+    },
+  };
+});
 
 export default IndexPage;
